c0df42bd81b021192757d4bcc12aa54218ecad79
Jan Remek <143747808+jan-remek@users.noreply.github.com>
2023-10-29 22:40:07 +0100

    Update questions.js
    New Questions

--- Data/questions.js
+++ Data/questions.js
@@ -2,61 +2,351 @@ export const questions = [
     {
         id: "XJ9F3PQK",
         category: "deep",
-        question: "Gibt es etwas was du nicht tust weil du es dir nicht zutraust, obwohl du dir vorstellen kannst dass es dich glücklich machen würde?"
+        question: "Gibt es etwas, was du nicht tust, weil du es dir nicht zutraust, obwohl du dir vorstellen kannst, dass es dich glücklich machen würde?"
     },
     {
-        id: "XJ9F3PQK",
+        id: "H9M5Q1SP",
         category: "deep",
-        question: "Wenn du von forne anfangen könntest, würdest du dich für den selben Weg (Karriere, Beruf etc.) entscheiden?"
+        question: "Wenn du von vorne anfangen könntest, würdest du dich für den selben Weg (Karriere, Beruf etc.) entscheiden?"
     },
     {
-        id: "A4BVKP6N",
-        category: "fun",
-        question: "Wenn Du für den Rest deines Lebens nur noch 5 Wörter / Ausdrücke verwenden dürftest, welche würdest du wählen?"
+        id: "Z5Y8PQ7R",
+        category: "deep",
+        question: "Glaubst du, dass wir als Menschen im Laufe der Zeit moralisch besser oder schlechter werden, oder glaubst du, dass sich nicht viel verändert hat?"
     },
     {
-        id: "M3S5E9HT",
-        category: "fun",
-        question: "Du bekommst eine SMS, eine Zombie-Pandemie ist ausgebrochen. Was machst Du in den ersten 24 Stunden?"
+        id: "D1U6M2VT",
+        category: "deep",
+        question: "Wenn du könntest, würdest du TikTok, Instagram und Co. abschaffen?"
     },
     {
-        id: "C6GZ2YFV",
-        category: "spicy",
-        question: "Welche Straftaten (laut Gesetz) hast Du bis heute begonnen?"
+        id: "G4H7J9KP",
+        category: "deep",
+        question: "Was ist deiner Meinung nach die schlimmste Erfindung der Menschheit?"
     },
     {
-        id: "L8N7QKRP",
-        category: "spicy",
-        question: "Was war die letzte Situation, in der Du einen vermeintlichen Fehler zugegeben hast nur um die Situation zu schlichten, obwohl Du dir eigentlich keiner Schuld bewusst warst?"
+        id: "L3W5B6TC",
+        category: "deep",
+        question: "Haben deine Eltern einen guten Job gemacht?"
     },
     {
-        id: "W9XVJ1DM",
-        category: "spicy",
-        question: "Hast Du schonmal deine:n Partner:in betrogen?"
+        id: "P2H6V4JS",
+        category: "deep",
+        question: "Wenn du die Macht hättest, eine Weltregel festzulegen, die von allen befolgt werden müsste, welche Regel würdest du einführen?"
     },
     {
-        id: "T2U4C5SA",
-        category: "spicy",
-        question: "Glaubst Du, das Du besser bist als andere Menschen?"
+        id: "K8R1Z7XM",
+        category: "deep",
+        question: "Inwieweit glaubst du, dass das Streben nach Wohlstand und materiellem Besitz das Glück beeinflusst?"
     },
     {
-        id: "H6P3Y8GQ",
-        category: "spicy",
-        question: "Wenn Du neben den 10 chrsitlichen Sünden eine weitere einführen müsstest, welche wäre das?"
+        id: "W7D3G5CT",
+        category: "deep",
+        question: "Welche Eigenschaft einer Person, mit der du dieses Spiel gerade spielst, würdest du gerne übernehmen?"
     },
     {
-        id: "R1K7M9ZX",
-        category: "spicy",
-        question: "Gibt es nach deiner Meinung eine Strafttat die die Todesstrafe gerechtfertigt?"
+        id: "Z6Y4L7BJ",
+        category: "deep",
+        question: "Würdest du 10 Jahre deines Lebens gegen Reichtum (ab jetzt) eintauschen?"
+    },
+    {
+        id: "H9M5Q1SP",
+        category: "deep",
+        question: "Glaubst du an Liebe auf den ersten Blick, oder denkst du, dass Liebe wachsen muss?"
+    },
+    {
+        id: "E3N7V6FX",
+        category: "deep",
+        question: "Glaubst du, dass es ein höheres Wesen oder eine höhere Macht gibt, die das Universum lenkt?"
+    },
+    {
+        id: "J1R8B2LW",
+        category: "deep",
+        question: "Glaubst du, dass Politiker mehr für das Gemeinwohl oder für ihre eigene Macht handeln?"
+    },
+    {
+        id: "C5K9A7HM",
+        category: "deep",
+        question: "Glaubst du, dass die Welt insgesamt besser oder schlechter wird?"
+    },
+    {
+        id: "S7E1T6DG",
+        category: "deep",
+        question: "Glaubst du, dass die Erziehung eines Kindes von der Gesellschaft oder von den Eltern stärker beeinflusst wird?"
+    },
+    {
+        id: "N4J6W1LB",
+        category: "deep",
+        question: "Gibt es Dinge, die du in deinem Leben bereust, aber nie zugegeben hast?"
+    },
+    {
+        id: "X9M6S3LK",
+        category: "deep",
+        question: "Gibt es etwas, neben deinem aktuellen Job, von dem du überzeugt bist, dass du darin eine erfolgreiche Karriere starten könntest? Wenn ja, was hält dich davon ab?"
+    },
+    {
+        id: "K1W2Z7TG",
+        category: "deep",
+        question: "Wenn du die Fähigkeit hättest, eine persönliche Eigenschaft oder Gewohnheit in einem Schlag zu ändern, welche würdest du wählen und warum?"
+    },
+    {
+        id: "U5H3N6ZP",
+        category: "deep",
+        question: "Gibt es Situationen, in denen du dich bewusst verstellst?"
+    },
+    {
+        id: "W9Y8P7JB",
+        category: "deep",
+        question: "Glaubst du, dass es Leid in einer Gesellschaft geben muss, damit sie sich weiterentwickeln kann?"
+    },
+    {
+        id: "A5P6F3HD",
+        category: "deep",
+        question: "Hast du jemals deine Eltern enttäuscht?"
     },
     {
         id: "Q4W6T2LB",
         category: "fun",
-        question: "Wovon kannst Du am meisten Aufzählen (z.B Harry Potter Charaktere)? Mache es vor!"
+        question: "Wovon kannst du am meisten aufzählen (z. B. Harry-Potter-Charaktere)? Mache es vor!"
+    },
+    {
+        id: "A4BVKP6N",
+        category: "fun",
+        question: "Wenn du für den Rest deines Lebens nur noch 5 Wörter / Ausdrücke verwenden dürftest, welche würdest du wählen?"
+    },
+    {
+        id: "M3S5E9HT",
+        category: "fun",
+        question: "Du bekommst eine SMS, eine Zombie-Pandemie ist ausgebrochen. Was machst du in den ersten 24 Stunden?"
+    },
+    {
+        id: "R9N1V7PA",
+        category: "fun",
+        question: "Wer von den hier Anwesenden würde in einer Zombieapokalypse am besten zurechtkommen und wer würde als erstes sterben?"
+    },
+    {
+        id: "B2A4K9XY",
+        category: "fun",
+        question: "Was war das seltsamste Gerücht über dich, das du je gehört hast, und wie kam es zustande?"
+    },
+    {
+        id: "P3M7D2VX",
+        category: "fun",
+        question: "Du musst in der Wildnis 30 Tage überleben und darfst nur 5 Dinge mitnehmen. Was nimmst du mit?"
+    },
+    {
+        id: "T9B6K1SP",
+        category: "fun",
+        question: "Du erhältst die Gelegenheit, mit einer berühmten Persönlichkeit einen Tag zu verbringen. Wer wäre das, und wie würdest du den Tag gestalten?"
+    },
+    {
+        id: "R2C4H8XF",
+        category: "fun",
+        question: "Die Bild schreibt einen Artikel über dich, wie lautet die Überschrift?"
+    },
+    {
+        id: "F5G7J1DL",
+        category: "fun",
+        question: "Wenn du müsstest, bei welcher Reality-Show würdest du mitmachen? Und welcher 'Kandidatentyp' wärst du in der Show?"
+    },
+    {
+        id: "D7V8Q4NP",
+        category: "fun",
+        question: "Wenn du das gesamte Wissen einer Person erhalten könntest, welche Person würdest du wählen?"
+    },
+    {
+        id: "E8N3H9KV",
+        category: "fun",
+        question: "Du bekommst die Möglichkeit, in die Vergangenheit zu reisen und eine persönliche Entscheidung zu ändern. Welche Entscheidung wählst du, und wie würde sich dein Leben dadurch verändern?"
+    },
+    {
+        id: "J5R4L7MP",
+        category: "fun",
+        question: "Was ist dein absoluter Traumjob (unabhängig von persönlichen Einschränkungen, die dich ggf. davon abhalten, wie z. B. Geld, Ausbildung, intellektuelle Kapazität etc.)?"
+    },
+    {
+        id: "C6T2F1XP",
+        category: "fun",
+        question: "Wenn du für den Rest deines Lebens nur noch eine 'Küche' essen dürftest (z. B. italienisch, griechisch etc.), welche wäre das?"
+    },
+    {
+        id: "U2H1M8LW",
+        category: "fun",
+        question: "Wenn dein Liebesleben ein Buchtitel wäre, wie würde er lauten?"
+    },
+    {
+        id: "V7G9B4KQ",
+        category: "fun",
+        question: "Wenn du einen erotischen Spitznamen für dich selbst wählen müsstest, welcher wäre es?"
+    },
+    {
+        id: "H8T2N1JC",
+        category: "fun",
+        question: "Wenn du (nur du) eine Information über den Köpfen der Menschen eingeblendet sehen könntest, welche wäre das?"
+    },
+    {
+        id: "T3H6C7ZK",
+        category: "fun",
+        question: "Gibt es ein Serienintro, das du jetzt sofort singen könntest?"
+    },
+    {
+        id: "K8R4W1BV",
+        category: "fun",
+        question: "Zähle innerhalb von 30 Sekunden, 25 Frauennamen auf"
+    },
+    {
+        id: "L8N7QKRP",
+        category: "spicy",
+        question: "Was war die letzte Situation, in der du einen vermeintlichen Fehler zugegeben hast, nur um die Situation zu schlichten, obwohl du dir eigentlich keiner Schuld bewusst warst?"
+    },
+    {
+        id: "W9XVJ1DM",
+        category: "spicy",
+        question: "Hast du schon einmal deinen Partner oder deine Partnerin betrogen?"
+    },
+    {
+        id: "T2U4C5SA",
+        category: "spicy",
+        question: "Glaubst du, dass du besser bist als andere Menschen?"
+    },
+    {
+        id: "H6P3Y8GQ",
+        category: "spicy",
+        question: "Wenn du neben den 10 Todsünden eine weitere einführen müsstest, welche wäre das?"
+    },
+    {
+        id: "R1K7M9ZX",
+        category: "spicy",
+        question: "Gibt es nach deiner Meinung eine Straftat, die die Todesstrafe rechtfertigt?"
     },
     {
         id: "E5N8D3PV",
         category: "spicy",
-        question: "Kann Prostitution oder Sexwork im Allgemeinen erfüllend sein?"
+        question: "Kann Prostitution oder Sexarbeit im Allgemeinen erfüllend sein?"
     },
+    {
+        id: "F8X2Z1ML",
+        category: "spicy",
+        question: "Denkst du, dass Menschen, die an spirituelle oder esoterische Ideen glauben, weniger intelligent sind?"
+    },
+    {
+        id: "M6S3K8FJ",
+        category: "spicy",
+        question: "Gibt es Umstände, in denen du kriminell werden würdest? Wenn ja, welche?"
+    },
+    {
+        id: "V7L2N4WT",
+        category: "spicy",
+        question: "Was war die letzte peinliche Situation, in der du dich befunden hast?"
+    },
+    {
+        id: "T9C5E1GQ",
+        category: "spicy",
+        question: "Was war dein größter finanzieller Fehler bis jetzt?"
+    },
+    {
+        id: "Q4U9N6HD",
+        category: "spicy",
+        question: "Glaubst du, dass es jemals gerechtfertigt ist, Geheimnisse vor deinem Partner zu haben, oder sollte in einer Beziehung alles offen und ehrlich sein?"
+    },
+    {
+        id: "X1T2V8KF",
+        category: "spicy",
+        question: "Würdest du eine Prostituierte daten?"
+    },
+    {
+        id: "V2P3F9MT",
+        category: "spicy",
+        question: "Würdest du jemals etwas Illegales tun, wenn du sicher wärst, nicht erwischt zu werden?"
+    },
+    {
+        id: "L8R9S2YF",
+        category: "spicy",
+        question: "Was ist das peinlichste oder beschämendste Erlebnis, das dir in der Öffentlichkeit passiert ist?"
+    },
+    {
+        id: "M1X7T4GK",
+        category: "spicy",
+        question: "Hattest du einmal in deinem Leben einen 'Feind' gehabt?"
+    },
+    {
+        id: "G9Q4K8DC",
+        category: "spicy",
+        question: "Welche gesellschaftlichen Normen oder Regeln findest du am sinnlosesten oder überflüssigsten?"
+    },
+    {
+        id: "B6W2F7JM",
+        category: "spicy",
+        question: "Hattest du schon einmal einen Schwarm auf eine fiktive Person? (z. B. Hermine aus Harry Potter)"
+    },
+    {
+        id: "Z8L1T5YV",
+        category: "spicy",
+        question: "Glaubst du, dass du in einer Krisensituation in der Lage wärst, unethische oder illegale Handlungen zu begehen?"
+    },
+    {
+        id: "Q4S6D3XN",
+        category: "spicy",
+        question: "Gibt es etwas von dir im Internet, was dir peinlich ist? Wenn ja, was?"
+    },
+    {
+        id: "Z4W7R9KG",
+        category: "spicy",
+        question: "Hast du in deinem Lebenslauf schon einmal gelogen?"
+    },
+    {
+        id: "M6C3V2BX",
+        category: "spicy",
+        question: "Hast du schon einmal mehr als eine Person gleichzeitig gedatet?"
+    },
+    {
+        id: "L1X8Y5GP",
+        category: "spicy",
+        question: "Was war das unangenehmste, was du betrunken gemacht hast?"
+    },
+    {
+        id: "S7J9K4VF",
+        category: "spicy",
+        question: "Hattest du schon einmal Sex mit jemandem und es dann bereut?"
+    },
+    {
+        id: "N2T3D6MW",
+        category: "spicy",
+        question: "Hattest du schon einmal eine negative Angelegenheit, die über einen Anwalt geklärt werden musste?"
+    },
+    {
+        id: "E9R4H1LB",
+        category: "spicy",
+        question: "Hast du dich schon einmal geprügelt?"
+    },
+    {
+        id: "W2F7P6XD",
+        category: "spicy",
+        question: "Hast du schon einmal beim ersten Date gelogen? Wenn ja, was war die Lüge?"
+    },
+    {
+        id: "D1G8X5MC",
+        category: "spicy",
+        question: "Hattest du schon einmal einen Dreier?"
+    },
+    {
+        id: "Y7B4K9SN",
+        category: "spicy",
+        question: "Hattest du schon einmal etwas mit jemandem und konntest dich danach nicht daran erinnern?"
+    },
+    {
+        id: "Q6L3T2HP",
+        category: "spicy",
+        question: "Wurdest du schon einmal aus einer Bar oder einem Club geworfen?"
+    },
+    {
+        id: "J9N1M7RW",
+        category: "spicy",
+        question: "Hast du schon einmal Sex abgebrochen, weil er so schlecht war?"
+    },
+    {
+        id: "P5V2S8FG",
+        category: "spicy",
+        question: "Hattest du schon einmal ein romantisches Verhältnis mit einem Vorgesetzten oder einer Vorgesetzten?"
+    }
 ];
